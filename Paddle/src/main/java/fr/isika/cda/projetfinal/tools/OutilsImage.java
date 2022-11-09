24353dc300df70d01f7b330254dffc0c53d2c62e
MbaAndjembe <andjembe_douglas@yahoo.fr>
2022-11-09 11:58:06 +0100

    suppresion des fichiers bootstrap et modification du fichier outilsImage

--- Paddle/src/main/java/fr/isika/cda/projetfinal/tools/OutilsImage.java
+++ Paddle/src/main/java/fr/isika/cda/projetfinal/tools/OutilsImage.java
@@ -11,29 +11,36 @@ import java.util.UUID;
 
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.Part;
-import javax.ws.rs.core.Context;
 
 public class OutilsImage {
 
+	private static final String RESOURCES_IMAGES = "/resources/images";
 	private static File fichierSauvegarde;
 
 	public static String sauvegarderImage(Part fichierEnvoye) throws IOException {
-		String[] nomFichierDecoupe = Paths.get(fichierEnvoye.getSubmittedFileName()).getFileName().toString().split("\\.");
-		String nomFichier = UUID.randomUUID().toString() + "." + nomFichierDecoupe[nomFichierDecoupe.length-1];
-		
+		Path fileName = Paths.get(fichierEnvoye.getSubmittedFileName()).getFileName();
+		String[] nomFichierDecoupe = fileName.toString().split("\\.");
+		String nomFichier = UUID.randomUUID().toString() + "." + nomFichierDecoupe[nomFichierDecoupe.length - 1];
+
 		ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
-        Object requestObj = context.getRequest();
-        HttpServletRequest request = (HttpServletRequest) requestObj;
-		
-		String dataDir = request.getSession().getServletContext().getRealPath("/resources/images");
-		
+		Object requestObj = context.getRequest();
+		HttpServletRequest request = (HttpServletRequest) requestObj;
+
+		String dataDir = request.getSession().getServletContext().getRealPath(RESOURCES_IMAGES);
+		File dataDirFile = new File(dataDir);
+		if (!dataDirFile.exists()) {
+			boolean created = dataDirFile.mkdirs();
+			if (!created) {
+				System.err.println("erreur création data dir for images");
+			}
+		}
+
 		fichierSauvegarde = new File(dataDir, nomFichier);
 		InputStream input = fichierEnvoye.getInputStream();
 		Files.copy(input, fichierSauvegarde.toPath());
-		return "resources/images/"+nomFichier;
+		return RESOURCES_IMAGES + "/" + nomFichier;
 	}
 	
 	public static void remplacerImage(Part fichierEnvoye, String cheminAncienFichier) throws IOException {
