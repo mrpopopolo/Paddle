84308b5f3472a71b13414ae32fa203d5ed9a5b7f
adriencr81 <107180086+adriencr81@users.noreply.github.com>
2022-11-17 12:41:38 +0100

    annonces

--- Paddle/src/main/java/fr/isika/cda/projetfinal/repositories/DemoBean.java
+++ Paddle/src/main/java/fr/isika/cda/projetfinal/repositories/DemoBean.java
@@ -57,7 +57,7 @@ public class DemoBean {
 		infosLogement.setMontantCharges(BigDecimal.valueOf(60.0));
 
 		InfosPerso infosPerso = new InfosPerso();
-		infosPerso.setNom("Dupont");
+		infosPerso.setNom("Dupond");
 		infosPerso.setPrenom("Jean");
 		infosPerso.setSexe("Homme");
 		infosPerso.setDateDeNaissance(LocalDate.of(1965, 10, 22));
@@ -85,8 +85,43 @@ public class DemoBean {
 		service.setDateEvenement(LocalDate.of(2022, 12, 02));
 		service.setImageService("resources/images/coursMath.jpeg");
 		service.setCreateurService(utilisateur);
-
+		
 		entityManager.persist(service);
+
+		
+		Service service2 = new Service();
+		service2.setTypeService(TypeService.SERVICE);
+		service2.setTitre("Cervelo S5 ");
+		service2.setDescription("A vendre, CERVELO S5 ULTEGRA DI2 12V, taille 56. ");
+		service2.setPrix(BigDecimal.valueOf(5000.00));
+		service2.setDateEvenement(LocalDate.of(2022, 12, 02));
+		service2.setImageService("resources/images/velo.JPG");
+		service2.setCreateurService(utilisateur);
+
+		entityManager.persist(service2);
+		
+		Service service3 = new Service();
+		service3.setTypeService(TypeService.SERVICE);
+		service3.setTitre("Télévision ");
+		service3.setDescription("Bonjour, je vends ma TV 32cm. ");
+		service3.setPrix(BigDecimal.valueOf(90.00));
+		service3.setDateEvenement(LocalDate.of(2022, 1, 17));
+		service3.setImageService("resources/images/tv.jpg");
+		service3.setCreateurService(utilisateur);
+
+		entityManager.persist(service3);
+		
+
+		Service service4 = new Service();
+		service4.setTypeService(TypeService.SERVICE);
+		service4.setTitre("Aston Martin Vantage 4,7l 426Ch ");
+		service4.setDescription("Magnifique Aston Martin V8 Vantage 4,7l, 426Ch, Noir Onyx, intérieur Cuir Obsidian Black. ");
+		service4.setPrix(BigDecimal.valueOf(58990.00));
+		service4.setDateEvenement(LocalDate.of(2022, 1, 17));
+		service4.setImageService("resources/images/aston martin.jpg");
+		service4.setCreateurService(utilisateur);
+
+		entityManager.persist(service4);
 
 		Reservation reservation = new Reservation();
 		reservation.setDateExecution(LocalDate.of(2022, 12, 02));
